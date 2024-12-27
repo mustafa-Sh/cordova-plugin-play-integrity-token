acfccbbd0f3b6f1a6f91280fff23e4ab094d7359
PeOmniBk <PeOmniBk@path-solutions.com>
2024-12-27 11:09:42 +0200

    combine two custom Cordova plugins

--- www/PlayIntegrityToken.js
+++ www/PlayIntegrityToken.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.integToken = function (arg0, arg1, success, error) {
     exec(success, error, 'PlayIntegrityToken', 'integToken', [arg0, arg1]);
 };
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'CordovaPluginIos', 'coolMethod', [arg0]);
+};
